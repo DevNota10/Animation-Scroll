f19ce0f13e2bbe5cc4fb38bba3050daec45e5e20
Kleber <dev.nota10.k@gmail.com>
2023-05-22 17:45:46 -0300

    Treino animation Scroll

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+const debounce = function(func,wait,immediate){
+    let timeout;
+    return function(...args){
+        const context =this;
+        const later = function(){
+            timeout = null;
+            if(!immediate) func.apply(context,args);
+        };
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later,wait);
+        if(callNow) func.apply(context,args);
+    };
+};
+const target = document.querySelectorAll('[data-anime]');
+
+const animateClas = 'animate';
+
+
+function animeScroll(){
+    const windowTop = window.scrollY  + ((window.innerHeight * 3) / 4); 
+
+    target.forEach((element)=>{
+       if((windowTop) > element.offsetTop){
+        element.classList.add('animate')
+       }
+       else{
+        element.classList.remove('animate')
+       }
+    })
+}
+
+animeScroll()
+
+if(target.length){
+
+    window.addEventListener('scroll', debounce(function(){
+        animeScroll()
+        console.log('foi')
+    }, 200));
+
+}
